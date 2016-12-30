3088f2d5994346ad9e224059d06f87b7b550f56d
Zhiwei Yang <yangzhiwei@youzan.com>
2016-12-30 19:52:52 +0800

    add mako

--- tmp.py
+++ tmp.py
@@ -1,6 +1,15 @@
 # coding: utf-8
 from mako.lookup import TemplateLookup
 from mako.template import Template
-mylookup = TemplateLookup(directories=['/Users/jerry/Library/Mobile Documents/com~apple~CloudDocs/python_love/learn_how_to_flask/mako'])
+from sqlalchemy import create_engine
+mylookup = TemplateLookup(directories=['./mako'])
 template = Template('<%include file="Hello.mako"/>', lookup=mylookup)
 template.render(name='xiaoMing')
+
+def orm():
+    engine = create_engine('sqlite://',echo=False)
+    with engine.connect() as con:
+        rs = con.execute('SELECT 1')
+        print rs.fetchone()
+if __name__ == '__main__':
+    orm()

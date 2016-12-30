e8c00fc59a38e6dcc346e4f2732625eeb8ae9a59
Zhiwei Yang <yangzhiwei@youzan.com>
2016-12-30 21:33:21 +0800

    v0.3

--- blog/app/__init__.py
+++ blog/app/__init__.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
 app.config.from_object("config")
+
+db = MongoEngine(app)
 
 from app import views, models
 

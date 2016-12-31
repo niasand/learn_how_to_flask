e9d9f82f46993178683d18f08bdb35360c01be92
Zhiwei Yang <yangzhiwei@youzan.com>
2016-12-31 10:53:15 +0800

    v0.7

--- blog/app/views.py
+++ blog/app/views.py
@@ -2,48 +2,55 @@
 
 from app import app
 from flask import render_template,request
-from models import Todo
+from models import Todo,TodoForm
+
 
 
 @app.route('/')
 def index():
+    form = TodoForm()
     todos = Todo.objects.all()
 
-    return render_template("index.html", todos=todos)
+    return render_template("index.html", todos=todos,form=form)
     # return render_template("index.html",text="Hello world")
 
-@app.route('/add', methods=['POST', ])
+@app.route('/add', methods=['POST', 'GET'])
 def add():
-    content = request.form.get("content")
-    todo = Todo(content=content)
-    todo.save()
+    # content = request.form.get("content")
+    form = TodoForm(request.form)
+    if form.validate():
+        content = form.content.data
+        todo = Todo(content=content)
+        todo.save()
 
     todos = Todo.objects.all()
-
-    return  render_template("index.html",todos=todos)
+    return  render_template("index.html",todos=todos,form=form)
 
 
 @app.route('/done/<string:todo_id>')
 def done(todo_id):
+    form = TodoForm()
     todo = Todo.objects.get_or_404(id=todo_id)
     todo.status = 1
     todo.save()
 
     todos = Todo.objects.all()
-    return render_template("index.html",todos=todos)
+    return render_template("index.html",todos=todos,form=form)
 
 @app.route('/undone/<string:todo_id>')
 def undone(todo_id):
+    form = TodoForm()
     todo = Todo.objects.get_or_404(id=todo_id)
     todo.status = 0
     todo.save()
 
     todos = Todo.objects.all()
-    return  render_template("index.html", todos=todos)
+    return  render_template("index.html", todos=todos,form=form)
 
 @app.route('/delete/<string:todo_id>')
 def delete(todo_id):
+    form = TodoForm()
     todo = Todo.objects.get_or_404(id=todo_id)
     todo.delete()
     todos = Todo.objects.all()
-    return render_template("index.html",todos=todos)
+    return render_template("index.html",todos=todos,form=form)
--- blog/config.py
+++ blog/config.py
@@ -1,1 +1,3 @@
+# SECRET_KEY = "never tell you"
 MONGODB_SETTINGS = {'DB': 'todo_db'}
+WTF_CSRF_ENABLED = False
